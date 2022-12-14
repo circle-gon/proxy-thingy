f093d6b12b71e170e3af68f3e38eaffa5f0bce2c
Glitch (glitch-hello-node) <none>
2022-12-14 15:56:47 +0000

    🚀🚞 Checkpoint ./server.js:37514084/2343

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-import express from "express"
-import morgan from "morgan"
-import {createProxyMiddleware } from 'http-proxy-middleware'
+import express from "express";
+import morgan from "morgan";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 // Create Express Server
 const app = express();
 
 // Configuration
 const PORT = 3000;
-const API_SERVICE_URL = "https://discord.com";
+const API_SERVICE_URL = "https://hidester.com";
+const middle = createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/`]: "",
+    },
+  });
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Info GET endpoint
-app.get('/info', (req, res, next) => {
-   res.send('This is a proxy service.');
+app.get("/info", (req, res, next) => {
+  res.send("This is a proxy service.");
 });
 
 // Proxy endpoints
-app.use('/', createProxyMiddleware({
-   target: API_SERVICE_URL,
-   changeOrigin: true,
-   pathRewrite: {
-       [`^/`]: '',
-   },
-}));
+app.use("/", (req, res, next) => {
+    middle(req, res, next).then(data => console.log(data))
+});
 // Start the Proxy
 app.listen(PORT, () => {
-   console.log(`Starting Proxy at port ${PORT}`);
+  console.log(`Starting Proxy at port ${PORT}`);
 });
