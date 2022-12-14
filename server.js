facd36825b7fee0ddfb5b0ba97b2e1318529911c
Glitch (glitch-hello-node) <none>
2022-12-14 18:22:22 +0000

    ⛸🎉 Checkpoint ./server.js:37514084/60

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Configuration
 const PORT = 3000;
-const API_SERVICE_URL = "https://www.proxysite.com";
+const API_SERVICE_URL = "https://www.4everproxy.com/";
 const middle = createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true,
