3c6bd32426289f90953a7d793e9a996e4c74eb28
Glitch (glitch-hello-node) <none>
2023-03-10 20:03:15 +0000

    🌂🔮 Checkpoint ./sw.js:37514084/1436

--- sw.js
+++ sw.js
@@ -1,10 +1,38 @@
 import {slice} from "./utils.js"
 
-const source = slice(document.location)
+const source = slice(document.pathname)[0]
+
+async function cloneRequest(request) {
+  const headers = {}
+  for (const [header, result] of request.headers.entries()) {
+    headers[header] = result
+  }
+  const copy = {
+    url: request.url,
+    headers: headers,
+    method: request.method,
+    mode: request.mode,
+    credentials: request.credentials,
+    cache: request.cache,
+    redirect: request.redirect,
+    referrer: request.referrer
+  };  
+  if (request.method !== 'GET' && request.method !== 'HEAD') {
+    const body = await request.clone().text()
+    copy.body = body
+  }
+  return copy
+}
+
+function transform(data) {
+  return new Request(data.url, data)
+}
+
+async function modifyRequest(request) {
+  const copy = await cloneRequest(request)
+  copy.
+}
 
 self.addEventListener("fetch", async e => {
-  const request = e.request
-  const url = new URL(request.url)
-  url.search = `?${URL.searchParams(`
-  const response = fetch(request.url)
+  e.respondWith(modifyRequest(e.request))
 })
