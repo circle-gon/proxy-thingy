127a4e81cb4a672a78597c6e728490457210b2b2
Glitch (glitch-hello-node) <none>
2023-04-07 18:44:35 +0000

    🙈🚏 Checkpoint ./server.js:37514084/11

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const options = {
       }
       return text;
     }*/
-    console.log("GET1: " + proxyRes.url);
+    console.log("GET1: " + req.url);
     return resBuffer;
   }),
   pathRewrite(path, req) {
