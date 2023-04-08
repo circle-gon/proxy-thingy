ac396ddcfa05cfa807887e36596462653c962fa5
Glitch (glitch-hello-node) <none>
2023-04-08 00:13:52 +0000

    💇⏰ Checkpoint ./replaceURL.js:37514084/1571

--- replaceURL.js
+++ replaceURL.js
@@ -2,32 +2,28 @@ import { proxyURL, slice, hasHTTPProtocol } from "./shared/utils.js";
 
 const origin = process.env.PROJECT_DOMAIN + ".glitch.me"
 
-
-
 function ignoreURL(url) {
   return false;
 }
 
 export function getCorrectURL(uri, currentURL) {
-  const urlified = new URL(uri)
-  const urlpath = currentURL.replace(new URL(currentURL))
+  //const curr = new URL(currentURL)
+  /*const path = curr.pathname
+  const base = curr.protocol + "//" + origin + slice(path)[0] + "/"*/
+  const newProtocol = new URL(uri).protocol + "//"
   if (ignoreURL(uri)) {
     // blacklisted urls
     return uri;
   } else if (uri.startsWith("//")) {
     // url to another page, starting with // 
-    // (technically a relative one but :shrug:)
-    const realURL = uri.replace("//", "")
+    const goTo = uri.replace("//", "")
     
-    // this url points to this page
-    if (realURL.startsWith(origin)) return uri
+    // points to itself
+    if (goTo.startsWith(origin)) return uri
     
-    return 
+    return proxyURL(newProtocol + goTo)
   } else if (uri.startsWith("/")) {
     // url to root
-    return (
-      origin + slice(new URL(currentURL))[0] + "/" + uri.replace("/", "")
-    );
   } else if (hasHTTPProtocol(uri)) {
     // url to another page, http or https
     // other protocols are outside this scope
