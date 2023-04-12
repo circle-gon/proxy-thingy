8fc9769bd1eeb1a5fe97ce60fba42b3a2e3c542b
Glitch (glitch-hello-node) <none>
2023-04-12 19:38:52 +0000

    🐓✋ Checkpoint ./static/sw.js:37514084/99 ./static/injection.js:37514084/2

--- static/injection.js
+++ static/injection.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then(r => {
       console.log('Service worker registered with scope: ', r.scope);
-      alert("Success! service worker loaded")
+      //alert("Success! service worker loaded")
     }, err => {
       alert("Failed to register service worker. Reason: " + err.toString())
       console.log('Service worker registration failed: ', err);
--- static/sw.js
+++ static/sw.js
@@ -1,3 +1,5 @@
+console.log("service worker go brrrrrrrr")
 self.addEventListener("fetch", e => {
+  console.log("SERVICE WORKER: request made")
   e.respondWith(fetch(e.request))
 })
