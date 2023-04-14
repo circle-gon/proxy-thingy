76894b49e5f3c2b33f9e7adf5a30f113d561fe58
Glitch (glitch-hello-node) <none>
2023-04-14 12:47:47 +0000

    🐓🚍 Checkpoint ./static/sw.js:37514084/871

--- static/sw.js
+++ static/sw.js
@@ -31,36 +31,14 @@ function replaceURL(originalURL, currentBase) {
   }
 }
 
-async function cacheFirst(request) {
-  // First try to get the resource from the cache
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-
-  // Next try to get the resource from the network
-  try {
-    const responseFromNetwork = await fetch(request);
-    // response may be used only once
-    // we need to save clone to put one copy in cache
-    // and serve second one
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
-  } catch (error) {
-    // when even the fallback response is not available,
-    // there is nothing we can do, but we must always
-    // return a Response object
-    return new Response("Network error happened", {
-      status: 408,
-      headers: { "Content-Type": "text/plain" },
-    });
-  }
-};
+async function mockClientRequest(request) {
+  
+}
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(deleteOldCaches());
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(cacheFirst(e.request))
+  e.respondWith(mockClientRequest(e.request))
 });
