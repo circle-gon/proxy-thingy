a66d890d37d122faea357aed9f62cb76136841d1
Glitch (glitch-hello-node) <none>
2023-04-15 14:13:54 +0000

    💝🌱 Checkpoint ./static/sw.js:37514084/6218

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,46 @@
-import { getFirst, isValidURL, proxyURL } from "./utils.js?proxyresource";
+//import { getFirst, isValidURL, proxyURL } from "./utils.js?proxyresource";
 
 //const CACHE_KEY = "v0.0.2";
+
+function hasHTTPProtocol(url) {
+  return url.startsWith("http://") || url.startsWith("https://");
+}
+
+function isValidURL(test) {
+  let url;
+  try {
+    url = new URL(test);
+  } catch (e) {
+    return false;
+  }
+  return hasHTTPProtocol(test);
+}
+function slice(url) {
+  return url.slice(1).split("/");
+}
+
+function getFirst(url) {
+  return decodeURIComponent(slice(url)[0]);
+}
+
+// forcing origin as an option is better because http/https differences
+// also less bad code/duplication
+
+// url should start with http vs https based on current page
+function proxyURL(url, origin) {
+  const originGo = new URL(url).origin;
+  // default to https, not that it matters anyway
+  // if you're using http you have a problem
+  return (
+    "https://" +
+    origin +
+    "/" +
+    encodeURIComponent(originGo) +
+    // prefer this over url.pathname as it always
+    // starts with /, even if you don't specify it
+    url.replace(originGo, "")
+  );
+}
 
 const DOMAIN = self.location.host;
 
@@ -27,17 +67,24 @@ function replaceURL(originalURL, currentBase) {
 
 async function mockClientRequest(request, id) {
   // 1. get the url originating the request
-  const clientURL = (await self.clients.get(id)).url;
-  
-  // 2. get a new url
-  const newURL = replaceURL(request.url, getFirst(new URL(clientURL).pathname));
-  
-  // 3. change the request
-  const r = new Request(newURL, request)
-  
-  // 4. return it
-  
-  return await fetch(r)
+  const clientURL = (await self.clients.get(id))?.url;
+
+  if (clientURL) {
+    // 2. get a new url
+    const newURL = replaceURL(
+      request.url,
+      getFirst(new URL(clientURL).pathname)
+    );
+    console.log(`Proxied url: ${request.url} -> ${newURL}`)
+
+    // 3. change the request
+    const r = new Request(newURL, request);
+
+    // 4. return it
+
+    return await fetch(r);
+  }
+  return await fetch(request);
 }
 
 self.addEventListener("activate", (e) => {
