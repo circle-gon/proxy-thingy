c33e006635a49ad7bbd42fd04ef5e900ee64b6b7
Glitch (adaptive-tricolor-whip) <none>
2023-09-29 20:13:05 +0000

    🚓❣️ Checkpoint ./static/selector.js:37514084/1095

--- static/selector.js
+++ static/selector.js
@@ -1,5 +1,14 @@
-const VUE_CDN = "https://cdn.jsdelivr.net/npm/vue@3.3.4"
+import { createApp } from "https://cdn.jsdelivr.net/npm/vue@3.3.4/+esm";
 
 const ConfigElement = class extends HTMLElement {
-  
-}
+  constructor() {
+    super();
+    this.hasInit = false;
+  }
+  connectedCallback() {
+    // this method may be called more than once
+    if (this.hasInit) return;
+    const shadow = this.attachShadow({mode: "closed"})
+    const app = createApp()
+  }
+};
